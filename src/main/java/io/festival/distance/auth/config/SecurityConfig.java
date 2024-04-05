ec8c675495402dbfd0170c45f41478ad0cb4c00d
JunRock <wnstjr120422@naver.com>
2024-04-05 13:57:10 +0900

    refactor : sercurity

--- src/main/java/io/festival/distance/auth/config/SecurityConfig.java
+++ src/main/java/io/festival/distance/auth/config/SecurityConfig.java
@@ -35,38 +35,40 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                // token을 사용하는 방식이기 때문에 csrf를 disable
-                .csrf().disable()
+            // token을 사용하는 방식이기 때문에 csrf를 disable
+            .csrf().disable()
 
-                .exceptionHandling()
-                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-                .accessDeniedHandler(jwtAccessDeniedHandler)
+            .exceptionHandling()
+            .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+            .accessDeniedHandler(jwtAccessDeniedHandler)
 
-                // enable h2-console
-                .and()
-                .headers()
-                .frameOptions()
-                .sameOrigin()
+            // enable h2-console
+            .and()
+            .headers()
+            .frameOptions()
+            .sameOrigin()
 
-                // 세션을 사용하지 않기 때문에 STATELESS로 설정
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            // 세션을 사용하지 않기 때문에 STATELESS로 설정
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
-                .and()
-                .addFilter(corsFilter) //@CrossOrigin(인증이 필요없는 곳에만 허용)
-                .formLogin().disable()
-                .httpBasic().disable()
-                .authorizeHttpRequests() // HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정
-                .antMatchers("/com/login").permitAll()
-                .antMatchers("/com/member/**").permitAll()
-                //.requestMatchers(PathRequest.toH2Console()).permitAll()// h2-console, favicon.ico 요청 인증 무시
-                .antMatchers("/h2-console/**").permitAll()
-                .antMatchers("/favicon.ico").permitAll()
-                .anyRequest().permitAll()
+            .and()
+            .addFilter(corsFilter) //@CrossOrigin(인증이 필요없는 곳에만 허용)
+            .formLogin().disable()
+            .httpBasic().disable()
+            .authorizeHttpRequests() // HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정
+            .antMatchers("/api/login").permitAll()
+            .antMatchers("/api/member/signup").permitAll()
+            //.requestMatchers(PathRequest.toH2Console()).permitAll()// h2-console, favicon.ico 요청 인증 무시
+            .antMatchers("/h2-console/**").permitAll()
+            .antMatchers("/favicon.ico").permitAll()
+            .antMatchers("/api/admin/signup").permitAll()
+            .anyRequest().authenticated()
 
-                .and()
-                .apply(new JwtSecurityConfig(tokenProvider)); // JwtFilter를 addFilterBefore로 등록했던 JwtSecurityConfig class 적용
+            .and()
+            .apply(new JwtSecurityConfig(
+                tokenProvider)); // JwtFilter를 addFilterBefore로 등록했던 JwtSecurityConfig class 적용
 
         return httpSecurity.build();
     }
