caf86d6f0cc8992efbf0b597380b0cea8798dc25
JunRock <wnstjr120422@naver.com>
2024-04-15 22:12:27 +0900

    refactor : STMP 비동기처리

--- src/main/java/io/festival/distance/DistanceApplication.java
+++ src/main/java/io/festival/distance/DistanceApplication.java
@@ -3,8 +3,11 @@ package io.festival.distance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class DistanceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DistanceApplication.class, args);
