a7d1208bdbc02a42593333dbe144fb63d772da47
JunRock <wnstjr120422@naver.com>
2024-04-16 16:41:21 +0900

    feat : #115 비밀번호 확인 API추가

--- src/main/java/io/festival/distance/domain/member/validsignup/ValidPassword.java
+++ src/main/java/io/festival/distance/domain/member/validsignup/ValidPassword.java
@@ -1,16 +1,24 @@
 package io.festival.distance.domain.member.validsignup;
 
+import io.festival.distance.domain.member.service.MemberService;
 import io.festival.distance.exception.DistanceException;
 import io.festival.distance.exception.ErrorCode;
+import java.security.Principal;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class ValidPassword {
-    public boolean duplicateCheckPassword(String password,String checkPassword){
-        if(!password.equals(checkPassword))
+
+    private final MemberService memberService;
+    private final PasswordEncoder encoder;
+
+    public void duplicateCheckPassword(Principal principal, String checkPassword) {
+        String password = memberService.findByTelNum(principal.getName()).getPassword();
+        if (!encoder.matches(checkPassword, password)) {
             throw new DistanceException(ErrorCode.NOT_CORRECT_PASSWORD);
-        return true;
+        }
     }
 }
