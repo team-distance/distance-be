0d2c5c0561827956ab669c9562455d9adae19fd9
JunRock <wnstjr120422@naver.com>
2024-04-28 01:11:13 +0900

    fix : 채팅방에 위치 정보가 0인 유저가 있는 경우 -1 반환

--- src/main/java/io/festival/distance/domain/gps/service/GpsProcessor.java
+++ src/main/java/io/festival/distance/domain/gps/service/GpsProcessor.java
@@ -12,13 +12,18 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class GpsProcessor {
+
     private final MemberService memberService;
+
     public double getDistance(long id1, long id2) {
         Member member1 = memberService.findMember(id1);
         Member member2 = memberService.findMember(id2);
+        if (member1.getLatitude() == 0 || member1.getLongitude() == 0 || member2.getLongitude() == 0
+            || member2.getLatitude() == 0){
+            return -1;
+        }
         double distance = calculateDistance(member1.getLatitude(), member1.getLongitude(),
             member2.getLatitude(), member2.getLongitude());
-        System.out.println(distance);
         return new BigDecimal(distance).setScale(2, RoundingMode.HALF_UP).doubleValue();
     }
 }
