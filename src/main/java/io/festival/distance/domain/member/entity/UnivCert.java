46efa6e5b4bcbcb528083564851c201ca6a58ef2
JunRock <wnstjr120422@naver.com>
2024-05-02 01:26:32 +0900

    refactor : UnivCert 추가

--- src/main/java/io/festival/distance/domain/member/entity/UnivCert.java
+++ src/main/java/io/festival/distance/domain/member/entity/UnivCert.java
@@ -10,7 +10,8 @@ public enum UnivCert {
     SUCCESS("SUCCESS","인증되었습니다!"),
     FAILED_1("FAILED_1","학생증 인증이 안되었습니다!"),
     FAILED_2("FAILED_2","사진이 흔들렸습니다!"),
-    FAILED_3("FAILED_3","해당 학교 학생증이 아닙니다!");
+    FAILED_3("FAILED_3","해당 학교 학생증이 아닙니다!"),
+    FAILED_4("FAILED_4","사용자가 등록한 성별과 다릅니다!");
     private final String type;
     private final String message;
 
