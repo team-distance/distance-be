46125ab777146540a9bf1cb833f489a19d16d34c
JunRock <wnstjr120422@naver.com>
2024-05-04 16:09:21 +0900

    refactor : request 컬럼 수정

--- src/main/java/io/festival/distance/domain/admin/adminfestival/foodtruck/dto/FoodTruckRequest.java
+++ src/main/java/io/festival/distance/domain/admin/adminfestival/foodtruck/dto/FoodTruckRequest.java
@@ -1,9 +1,6 @@
 package io.festival.distance.domain.admin.adminfestival.foodtruck.dto;
 
-import org.springframework.web.multipart.MultipartFile;
-
 public record FoodTruckRequest(
-    MultipartFile file,
     String truckName,
     String description,
     String school
