22221dfd3b2802d4de926dd9a9c1045764e9e5a4
JunRock <wnstjr120422@naver.com>
2024-05-19 18:07:07 +0900

    feat : Master Slave를 통한 DB 분산

---
+++ src/main/java/io/festival/distance/infra/jpa/DataSourceConfiguration.java
@@ -0,0 +1,38 @@
+package io.festival.distance.infra.jpa;
+
+
+import static io.festival.distance.infra.jpa.config.MasterDataSource.DISTANCE_MASTER_DATASOURCE;
+import static io.festival.distance.infra.jpa.config.SlaveDataSource.DISTANCE_SLAVE_DATASOURCE;
+
+import java.util.Map;
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+
+@Configuration
+@RequiredArgsConstructor
+public class DataSourceConfiguration {
+
+    private final DataSourceCreator dataSourceCreator;
+
+    @Bean
+    public DataSource routingDataSource(
+        @Qualifier(DISTANCE_MASTER_DATASOURCE) DataSource masterDataSource,
+        @Qualifier(DISTANCE_SLAVE_DATASOURCE) DataSource slaveDataSource
+    ) {
+        Map<Object, Object> immutableDataSourceMap = dataSourceCreator.createImmutableDataSource(
+            masterDataSource, slaveDataSource
+        );
+        return dataSourceCreator.createRoutingDataSource(immutableDataSourceMap, masterDataSource);
+    }
+
+    @Primary
+    @Bean
+    public DataSource dataSource(@Qualifier("routingDataSource") DataSource routingDataSource) {
+        return new LazyConnectionDataSourceProxy(routingDataSource);
+    }
+}
---
+++ src/main/java/io/festival/distance/infra/jpa/JpaConfiguration.java
@@ -0,0 +1,62 @@
+package io.festival.distance.infra.jpa;
+
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "io.festival.distance")
+public class JpaConfiguration {
+    private static final String BASE_PACKAGE = "io.festival.distance";
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+        @Qualifier("dataSource") DataSource dataSource) {
+
+        LocalContainerEntityManagerFactoryBean entityManagerFactory
+            = new LocalContainerEntityManagerFactoryBean();
+
+        entityManagerFactory.setDataSource(dataSource);
+        entityManagerFactory.setPackagesToScan(BASE_PACKAGE);
+        entityManagerFactory.setJpaVendorAdapter(jpaVendorAdapter());
+        entityManagerFactory.setPersistenceUnitName("entityManager");
+        entityManagerFactory.setJpaProperties(hibernateProperties());
+
+        return entityManagerFactory;
+
+    }
+
+    private JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        hibernateJpaVendorAdapter.setShowSql(false);
+        hibernateJpaVendorAdapter.setDatabase(Database.MYSQL);
+        hibernateJpaVendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+        return hibernateJpaVendorAdapter;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty("hibernate.format_sql", "true");
+        return properties;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(
+        @Qualifier("entityManagerFactory") LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(entityManagerFactory.getObject());
+        return jpaTransactionManager;
+    }
+}
