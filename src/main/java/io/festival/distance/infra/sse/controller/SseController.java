cc7e0d6e2023432a2fba8f44f62d722121eca684
JunRock <wnstjr120422@naver.com>
2024-05-24 23:37:31 +0900

    fix : sse 파라미터 토큰으로 변경

--- src/main/java/io/festival/distance/infra/sse/controller/SseController.java
+++ src/main/java/io/festival/distance/infra/sse/controller/SseController.java
@@ -1,8 +1,10 @@
 package io.festival.distance.infra.sse.controller;
 
+import io.festival.distance.auth.jwt.TokenProvider;
 import io.festival.distance.domain.conversation.waiting.service.ChatWaitingService;
 import io.festival.distance.infra.sse.service.SseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -20,9 +23,12 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 public class SseController {
     private final SseService sseService;
     private final ChatWaitingService chatWaitingService;
-
+    private final TokenProvider jwtTokenProvider;
     @GetMapping(value = "/subscribe/{memberId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public ResponseEntity<SseEmitter> subscribe(@PathVariable Long memberId) {
+    public ResponseEntity<SseEmitter> subscribe(@PathVariable Long memberId, @RequestParam("token") String token) {
+        if (!jwtTokenProvider.validateToken(token,"ACCESS")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
         return ResponseEntity.ok(sseService.subscribe(memberId));
     }
 
