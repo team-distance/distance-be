1978e9df2824a4ca025f193579edfdd22c702a99
JunRock <wnstjr120422@naver.com>
2024-08-30 16:21:27 +0900

    feat : 이미지 우선순위 설정

--- src/main/java/io/festival/distance/domain/councilimage/dto/response/CouncilImageResponse.java
+++ src/main/java/io/festival/distance/domain/councilimage/dto/response/CouncilImageResponse.java
@@ -4,10 +4,11 @@ import io.festival.distance.domain.councilimage.entity.CouncilImage;
 import lombok.Builder;
 
 @Builder
-public record CouncilImageResponse(String imageUrl) {
+public record CouncilImageResponse(String imageUrl, int priority) {
     public static CouncilImageResponse toCouncilImageResponse(CouncilImage councilImage){
         return CouncilImageResponse.builder()
             .imageUrl(councilImage.getImageUrl())
+            .priority(councilImage.getPriority())
             .build();
     }
 }
