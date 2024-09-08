a51e3288cd62f0ea6e6f634fcb3e248c793d9559
JunRock <wnstjr120422@naver.com>
2024-09-08 16:11:21 +0900

    refactor : 중복 코드 리팩토링

--- src/main/java/io/festival/distance/domain/statistics/service/StatisticsService.java
+++ src/main/java/io/festival/distance/domain/statistics/service/StatisticsService.java
@@ -1,9 +1,6 @@
 package io.festival.distance.domain.statistics.service;
 
-import static io.festival.distance.utils.DateUtil.getEndOfMonth;
-import static io.festival.distance.utils.DateUtil.getEndOfWeek;
-import static io.festival.distance.utils.DateUtil.getStartOfMonth;
-import static io.festival.distance.utils.DateUtil.getStartOfWeek;
+import static io.festival.distance.domain.statistics.service.serviceimpl.StatisticsTypeCheck.result;
 
 import io.festival.distance.domain.member.entity.Member;
 import io.festival.distance.domain.member.service.serviceimpl.MemberReader;
@@ -11,10 +8,10 @@ import io.festival.distance.domain.statistics.dto.response.CountResponse;
 import io.festival.distance.domain.statistics.dto.response.StatisticsResponse;
 import io.festival.distance.domain.statistics.entity.CouncilStatistics;
 import io.festival.distance.domain.statistics.repository.StatisticsRepository;
+import io.festival.distance.domain.statistics.service.serviceimpl.StatisticsTypeCheck;
 import io.festival.distance.domain.statistics.service.serviceimpl.StatisticsValidator;
 import io.festival.distance.domain.studentcouncil.entity.StudentCouncil;
 import io.festival.distance.domain.studentcouncil.service.serviceimpl.CouncilReader;
-import io.festival.distance.infra.redis.statistics.Statistics;
 import io.festival.distance.infra.redis.statistics.StatisticsReader;
 import java.time.LocalDate;
 import java.util.Optional;
@@ -42,26 +39,11 @@ public class StatisticsService {
         StudentCouncil studentCouncil = councilReader.findStudentCouncil(councilId);
         Member member = memberReader.findTelNum(telNum);
         statisticsValidator.validateAuthority(studentCouncil, member);
-        LocalDate startDate;
-        LocalDate endDate;
-        switch (type) {
-            case "daily" -> {
-                startDate = date;
-                endDate = date;
-            }
-            case "weekly" -> {
-                startDate = getStartOfWeek(date);
-                endDate = getEndOfWeek(date);
-            }
-            case "monthly" -> {
-                startDate = getStartOfMonth(date);
-                endDate = getEndOfMonth(date);
-            }
-            default -> {
-                return null;
-            }
-        }
+
+        StatisticsTypeCheck result = result(date, type);
+
         Integer count = statisticsReader.findStatistics(councilId).getCount();
+
         Optional<CouncilStatistics> statistics =
             statisticsRepository.findByDateAndStudentCouncil(LocalDate.now(), studentCouncil)
                 .or(() -> {
@@ -80,38 +62,24 @@ public class StatisticsService {
         //최종 조회수 측정
         return statisticsRepository.findByStudentCouncilAndDateBetween(
                 studentCouncil,
-                startDate,
-                endDate
+                result.startDate(),
+                result.endDate()
             )
-            .map(it -> StatisticsResponse.fromCount(startDate, it))
-            .orElse(StatisticsResponse.fromCount(startDate, 0));
+            .map(it -> StatisticsResponse.fromCount(result.startDate(), it))
+            .orElse(StatisticsResponse.fromCount(result.startDate(), 0));
     }
 
     public StatisticsResponse checkTotalStatistics(String type, LocalDate date, String telNum) {
         Member member = memberReader.findTelNum(telNum);
-        LocalDate startDate;
-        LocalDate endDate;
-        switch (type) {
-            case "daily" -> {
-                startDate = date;
-                endDate = date;
-            }
-            case "weekly" -> {
-                startDate = getStartOfWeek(date);
-                endDate = getEndOfWeek(date);
-            }
-            case "monthly" -> {
-                startDate = getStartOfMonth(date);
-                endDate = getEndOfMonth(date);
-            }
-            default -> {
-                return null;
-            }
-        }
+        StatisticsTypeCheck result = result(date, type);
 
-        return statisticsRepository.findByDateBetween(startDate, endDate, member.getAuthority())
-            .map(it -> StatisticsResponse.fromCount(startDate,it))
-            .orElse(StatisticsResponse.fromCount(startDate,0));
+        return statisticsRepository.findByDateBetween(
+                result.startDate(),
+                result.endDate(),
+                member.getAuthority()
+            )
+            .map(it -> StatisticsResponse.fromCount(result.startDate(), it))
+            .orElse(StatisticsResponse.fromCount(result.startDate(), 0));
     }
 
     public CountResponse calculateTotalCount(String telNum) {
