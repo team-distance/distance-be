decfff76fcd47149cc932bd504ffbdb4961a7cdd
JunRock <wnstjr120422@naver.com>
2024-09-08 20:56:24 +0900

    feat : 총학 게시글 통계 일급컬렉션 적용

---
+++ src/main/java/io/festival/distance/domain/statistics/service/serviceimpl/StatisticsUpdater.java
@@ -0,0 +1,38 @@
+package io.festival.distance.domain.statistics.service.serviceimpl;
+
+import io.festival.distance.domain.statistics.entity.CouncilStatistics;
+import io.festival.distance.domain.statistics.repository.StatisticsRepository;
+import io.festival.distance.domain.studentcouncil.entity.StudentCouncil;
+import io.festival.distance.domain.studentcouncil.service.serviceimpl.CouncilReader;
+import io.festival.distance.infra.redis.statistics.StatisticsDeleter;
+import java.time.LocalDate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class StatisticsUpdater {
+    private final StatisticsRepository statisticsRepository;
+    private final CouncilReader councilReader;
+    private final StatisticsDeleter statisticsDeleter;
+
+    @Transactional
+    public void update(Long councilId, int count ){
+        StudentCouncil studentCouncil = councilReader.findStudentCouncil(councilId);
+        CouncilStatistics statistics =
+            statisticsRepository.findByDateAndStudentCouncil(LocalDate.now(),
+                    studentCouncil)
+                .orElseGet(() -> {
+                    CouncilStatistics councilStatistics = CouncilStatistics.builder()
+                        .count(1)
+                        .date(LocalDate.now())
+                        .studentCouncil(studentCouncil)
+                        .build();
+                    statisticsRepository.save(councilStatistics);
+                    return councilStatistics;
+                });
+        statistics.updateCount(count);
+        statisticsDeleter.delete(councilId);
+    }
+}
