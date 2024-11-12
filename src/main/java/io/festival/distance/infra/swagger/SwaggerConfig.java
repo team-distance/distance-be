01ed7d6a2d064ae5885e989ff863ee51e356ea50
JunRock <wnstjr120422@naver.com>
2024-11-12 21:52:26 +0900

    refactor : swagger 접근권한 설정

--- src/main/java/io/festival/distance/infra/swagger/SwaggerConfig.java
+++ src/main/java/io/festival/distance/infra/swagger/SwaggerConfig.java
@@ -7,8 +7,10 @@ import java.util.List;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class SwaggerConfig {
 
     @Bean
