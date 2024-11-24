f08a3e51f868c87a2408ea02e16e6ebd35a5574d
JunRock <wnstjr120422@naver.com>
2024-11-24 17:44:53 +0900

    refactor : unused import delete

--- src/main/java/io/festival/distance/infra/s3/config/S3Config.java
+++ src/main/java/io/festival/distance/infra/s3/config/S3Config.java
@@ -8,10 +8,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Configuration
