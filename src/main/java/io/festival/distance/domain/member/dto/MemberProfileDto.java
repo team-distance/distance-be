c51bbc30a05d74f36cdf29cdde8e017d0c44c870
JunRock <wnstjr120422@naver.com>
2024-11-28 20:26:56 +0900

    refactor : 멤버 프로필 조회 시 닉네임 추가

--- src/main/java/io/festival/distance/domain/member/dto/MemberProfileDto.java
+++ src/main/java/io/festival/distance/domain/member/dto/MemberProfileDto.java
@@ -4,9 +4,13 @@ import java.util.List;
 import lombok.Builder;
 
 @Builder
-public record MemberProfileDto(String mbti,
-                               String memberCharacter,
-                               String department,
-                               List<MemberTagDto> memberTagDto,
-                               List<MemberHobbyDto> memberHobbyDto) {
+public record MemberProfileDto(
+    String mbti,
+    String memberCharacter,
+    String nickName,
+    String department,
+    List<MemberTagDto> memberTagDto,
+    List<MemberHobbyDto> memberHobbyDto
+) {
+
 }
